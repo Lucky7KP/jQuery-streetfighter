555779fa76148085a495010277e40c38c4c47db7
Lucky7KP <kpeltier@Kristinas-MacBook-Pro.local>
2014-06-29 13:06:41 -0700

    included background and instructions

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 		$(".ryu-throwing").show();
 		$(".hadouken").finish().show();
 		$(".hadouken").animate(
-				{"left": "850px"}, 
+				{"left": "800px"}, 
 				500,
 				function() {
 					$(this).hide();
@@ -24,6 +24,22 @@ $(document).ready(function() {
 	.mouseup(function() {
 		$(".ryu-throwing").hide();
 		$("ryu-ready").show();
+	});
+	$("body").keydown(function(event) {
+		if (event.keyCode == 88) {
+			$(".ryu-cool").show();
+			$(".ryu-still").hide();
+			$("ryu-ready").hide();
+			$("rhy-throwing").hide();
+		}
+	})
+	.keyup(function(event) {
+		if (event.keyCode == 88) {
+			$(".ryu-cool").hide();
+			$(".ryu-still").show();
+			$("ryu-ready").hide();
+			$("rhy-throwing").hide();
+		}	
 	});
 });
 function playHadouken () {
