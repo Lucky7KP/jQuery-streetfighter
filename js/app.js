c21990f710952dd01d1d8e912a54cf79a71187d6
Lucky7KP <kpeltier@Kristinas-MacBook-Pro.local>
2014-06-29 14:14:10 -0700

    changed volume code

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
 	});
 });
 function playHadouken () {
-	$("#hadouken-sound")[0].volune = 0.5;
+	$("#hadouken-sound")[0].volume = 0.5;
 	$("#hadouken-sound")[0].load()
 	$("#hadouken-sound")[0].play();
 }
